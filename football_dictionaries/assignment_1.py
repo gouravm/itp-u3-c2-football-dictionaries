58f77d6ccf54b9ae20097f038368ba823f950482
gourav <appdevstr@gmail.com>
2019-01-29 07:58:56 +0000

    Finished assignment 1

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,18 @@
 def players_as_dictionaries(squads_list):
-    pass
+    myli = []
+    for players in squads_list:
+        mydict = {
+            'caps': players[4],
+            'club': players[5],
+            'club_country': players[7],
+            'country': players[6],
+            'date_of_birth': players[3],
+            'name': players[2],
+            'number': players[0],
+            'position': players[1],
+            'year': players[8]
+        }
+        myli.append(mydict)
+        
+    print(myli)
+    return myli
