8e31d2ec729a3e5e5ca1746a6db0b64ab4180c2c
gourav <appdevstr@gmail.com>
2019-01-29 19:41:05 +0000

    finished assignment 2

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,13 @@
+from football_dictionaries.assignment_1 import players_as_dictionaries
+
 def players_by_position(squads_list):
-    pass
+    mydict = players_as_dictionaries(squads_list)
+    newdict = {}
+    
+    for players in mydict:
+        group_by_position = players['position']
+        newdict.setdefault(group_by_position,[])
+        newdict[group_by_position].append(players)
+        
+    return newdict
+    
